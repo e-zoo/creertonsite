473ddc3c1cbf50919e68346441e281ac66be0332
infinitydev4 <infinitydev4@gmail.com>
2018-11-02 18:03:03 -0700

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require js/form-validator.min
+//= require js/jquery.countdown.min
+//= require js/jquery.countTo
+//= require js/jquery.mixitup
+//= require js/jquery.inview.min
+//= require js/jquery.magnific-popup.min
+
+//= require popper
+//= require js/bootstrap.min
+//= require js/particles.min
+//= require particlesRun
+//= require js/style-switcher
+//= require js/scrollreveal.min
+
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
